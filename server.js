851c1f3b99c16045bfcd990df9bf7c98c92c49a9
Fabian Pierre Peña Jacobo <f.pena.jacobo@gmail.com>
2016-08-16 14:20:21 -0500

    Added configuration parser

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require('express');
-var app = express();
+var bodyparser = require('body-parser');
 
+
+var app = express();
 
 app.set('port', (process.env.PORT || 8080));
 app.use(express.static(__dirname + '/public'));
+
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json());
+
 app.use('/scripts', express.static(__dirname + '/node_modules/'));
 app.use(function(req, res) {
 	res.sendFile(__dirname + '/public/index.html');
